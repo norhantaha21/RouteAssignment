3b7978dcfab9db2f272582ef0000d01718afdedf
nourhantaha21 <norhantaha43@gmail.com>
2025-09-29 20:48:31 +0300

    Add files via upload

---
+++ FavFoodTask/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace FavoriteFoodApp
+{
+    internal class Program
+    {
+
+        enum Food
+        {
+            Pizza,
+            Burger,
+            Salad
+        }
+
+        static void Main(string[] args)
+        {
+                
+                Console.Write("Enter your favorite food (Pizza, Burger, Salad): ");
+                string input = Console.ReadLine();
+
+                bool success = Enum.TryParse<Food>(input, true, out Food result);
+
+                if (success)
+                {
+                    Console.WriteLine($"You chose: {result}");
+                }
+                else
+                {
+                    Console.WriteLine("That food is not on the menu!");
+                }
+
+            }
+        }
+}
