4d2ce8540ee9477d34f9cd2749f6a38ca30be149
nourhantaha21 <norhantaha43@gmail.com>
2025-11-29 18:17:51 +0200

    Add files via upload

---
+++ EfcoreSession04/InheritanceMapping/AppDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using static InheritanceMapping.Models;
+
+namespace InheritanceMapping
+{
+    internal class AppDbContext : DbContext
+    {
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlServer("Server=Localhost;Database=InheritencaMapping;Trusted_Connection=true;TrustServerCertificate=true");
+            }
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                // Q1: Vehicle TPH
+                modelBuilder.Entity<Vehicle>()
+                    .HasDiscriminator<string>("VehicleType")
+                    .HasValue<Vehicle>("Vehicle")
+                    .HasValue<Car>("Car")
+                    .HasValue<Bus>("Bus");
+
+                // Q2: Payment TPT
+                modelBuilder.Entity<Payment>().ToTable("Payments");
+                modelBuilder.Entity<CreditCardPayment>().ToTable("CreditCardPayments");
+                modelBuilder.Entity<CashPayment>().ToTable("CashPayments");
+
+                //Q3: Product TPC
+                modelBuilder.Entity<Book>().ToTable("Books");
+                modelBuilder.Entity<Electronics>().ToTable("Electronics");
+            }
+        }
+    }
+
---
+++ EfcoreSession04/InheritanceMapping/Models.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceMapping
+{
+    internal class Models
+    {
+            // Q1
+            public class Vehicle
+            {
+                public int Id { get; set; }
+                public string Model { get; set; }
+                public int Speed { get; set; }
+            }
+
+            public class Car : Vehicle
+            {
+                public int NumberOfDoors { get; set; }
+            }
+
+            public class Bus : Vehicle
+            {
+                public int Capacity { get; set; }
+            }
+
+
+            //Q2
+            public class Payment
+            {
+                public int Id { get; set; }
+                public int Amount { get; set; }
+            }
+
+            public class CreditCardPayment : Payment
+            {
+                public int CardNumber { get; set; }
+            }
+
+            public class CashPayment : Payment
+            {
+                public string Currency { get; set; }
+            }
+
+            //Q3
+            public class Product
+            {
+                public int Id { get; set; }
+                public string Name { get; set; }
+            }
+
+            public class Book : Product
+            {
+                public string Author { get; set; }
+            }
+
+        public class Electronics : Product
+        {
+            public string Prand { get; set; }
+        
+          }
+        }
+}
